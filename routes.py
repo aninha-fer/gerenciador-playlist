30ccfa9170cfc652705b7f7291d0ab9e859e24b7
Ana Carolina <anafer.digital@gmail.com>
2025-11-27 20:38:06 -0300

    front

--- routes.py
+++ routes.py
@@ -4,4 +4,6 @@ from musicas import musicas_para_projeto
 
 @app.route("/")
 def home():
-    return render_template("home.html", musicas=musicas_para_projeto)
+    playlist = musicas_para_projeto[:10]  # Primeiras 10 músicas para a playlist
+    recomendadas = musicas_para_projeto[10:]  # Restantes para recomendações
+    return render_template("home.html", playlist=playlist, recomendadas=recomendadas)
